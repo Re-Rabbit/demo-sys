0c8d85595078f96f67f04e90627958efcb0cd6e6
yuffiy <yfhj1990@hotmail.com>
2016-08-11 19:35:21 +0800

    update view and row

--- index.js
+++ index.js
@@ -98,9 +98,122 @@ ProdSpecRow.of = function(opt) {
   return new ProdSpecRow(opt)
 }
 
-//window.lf = LabelField.of({ $el: $('.js-labelfield') })
-window.psr = ProdSpecRow.of({ $el: $('.js-prodspecview') })
 
+const ProdSpecViewTpl = id => `
+<div data-id=${id} class="js-prodspecview-row"></div>
+`
+
+
+class ProdSpecView {
+  constructor({ $el, row = 3 }) {
+    this.$el = $el
+    this.row = row
+
+    this.datastate = {}
+
+    this.init()
+  }
+
+  createRow() {
+    let id = Date.now()
+
+    let $row = $(ProdSpecViewTpl(id)).appendTo(this.$body)
+
+    let data = {
+      $row: $row,
+      row: ProdSpecRow.of({ $el: $row })
+    }
+
+    this.datastate.rows[id] = data
+  }
+
+  getVal() {
+
+  }
+
+  init() {
+
+    let { $el, datastate, createRow } = this
+
+    this.$create = $el.find('.js-prodspecview-createrow')
+    this.$enable = $el.find('.js-prodspecview-enable')
+    this.$body = $el.find('.js-prodspecview-body')
+
+    this.datastate.rows = {}
+    createRow.bind(this)()
+
+    this.on = $el.on.bind($el)
+    this.trigger = $el.trigger.bind($el)
+
+    this.event()
+  }
+
+  event() {
+
+    let {
+      $el,
+      $create,
+      $enable,
+      $body,
+      row,
+      datastate,
+      createRow,
+      trigger,
+      on
+    } = this
+
+    createRow = createRow.bind(this)
+
+    function exports() {
+
+    }
+
+
+    $create.on('click', function() {
+      if(Object.keys(datastate.rows).length >= 3) return
+      createRow()
+
+      trigger('prodspecview.export', datastate)
+    })
+
+
+    $el.on('prodspecrow.removerow', '.js-prodspecview-row', function() {
+      if(Object.keys(datastate.rows).length <= 1) return
+      let id = $(this).data('id')
+      datastate.rows[id].$row.remove()
+      delete datastate[id]
+
+      trigger('prodspecview.export', datastate)
+    })
+
+    $el.on('prodspecrow.export', '.js-prodspecview-row', function() {
+      trigger('prodspecview.export', datastate)
+    })
+
+
+    $enable.on('change', function() {
+      datastate.checked = $(this).prop('checked')
+
+      trigger('prodspecview.export', datastate)
+    })
+
+  }
+}
+
+ProdSpecView.of = function(opt) {
+  return new ProdSpecView(opt)
+}
+
+//window.lf = LabelField.of({ $el: $('.js-labelfield') })
+//window.psr = ProdSpecRow.of({ $el: $('.js-prodspecview') })
+/*
 psr.on('prodspecrow.export', function(evt, data) {
+  console.log(data)
+})
+*/
+
+
+window.psv = ProdSpecView.of({ $el: $('.js-prodspecview') })
+psv.on('prodspecview.export', function(evt, data) {
   console.log(data)
 })
